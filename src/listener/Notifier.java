748ebe2eedbb4a90dca4ae9af7f36720a3b81dcd
Lukas Rose <lukas.rose@gmx.de>
2015-06-20 11:05:49 +0200

    Renaming NetworkClient to NetworkServer, since it actually IS the server.

--- src/listener/Notifier.java
+++ src/listener/Notifier.java
@@ -1,25 +1,25 @@
 package listener;
 
-import network.NetworkClient;
+import network.NetworkServer;
 import logging.Logger;
 
 public class Notifier implements network.NetworkNotifier {
 	private boolean bidirectional = false;
-	private NetworkClient client = null;
+	private NetworkServer server = null;
 	
 	public Notifier(){
 		
 	}
 	
-	public void setClient(NetworkClient client){
-		this.client = client;
+	public void setServer(NetworkServer server){
+		this.server = server;
 		bidirectional = true;
 	}
 	
 	public void onNotify(String notifyString) {
 		Logger.logMessage('I', this, "passed:" + notifyString);
-		if (bidirectional = true && client != null){
-			client.send(notifyString);
+		if (bidirectional = true && server != null){
+			server.send(notifyString);
 		}
 	}
 }
--- src/main/Main.java
+++ src/main/Main.java
@@ -1,14 +1,14 @@
 package main;
 
 import listener.Notifier;
-import network.NetworkClient;
+import network.NetworkServer;
 import network.NetworkNotifier;
 
 public class Main {
 	public static void main(String[] args){
 		Notifier n = new Notifier();
-		NetworkClient nc = new NetworkClient(n, 1234);
-		nc.start();
-		n.setClient(nc); //gets executed, because NetworkListener is a Thread
+		NetworkServer ns = new NetworkServer(n, 1234);
+		ns.start();
+		n.setServer(ns); //gets executed, because NetworkListener is a Thread
 	}
 }
