0e8105a8dd80771f08ed516f9abe9edd28329d71
Lukas Rose <lukas.rose@gmx.de>
2015-06-20 11:19:26 +0200

    Basic NetworkClient implemented

--- src/main/Main.java
+++ src/main/Main.java
@@ -2,13 +2,17 @@ package main;
 
 import listener.Notifier;
 import network.NetworkServer;
+import network.NetworkClient;
 import network.NetworkNotifier;
 
 public class Main {
 	public static void main(String[] args){
 		Notifier n = new Notifier();
 		NetworkServer ns = new NetworkServer(n, 1234);
+		NetworkClient nc = new NetworkClient(n, "localhost", 1234);
 		ns.start();
 		n.setServer(ns); //gets executed, because NetworkListener is a Thread
+		nc.send("Test!");
+		nc.send("Asdf!");
 	}
 }
