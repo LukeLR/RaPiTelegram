12a4c71d9246b98887b0ae6a3965ac03ba06f287
Lukas Rose <lukas.rose@gmx.de>
2015-06-29 00:40:01 +0200

    Message class

--- src/misc/Message.java
+++ src/misc/Message.java
@@ -10,5 +10,82 @@ public class Message {
 	protected boolean out = false;
 	protected boolean unread = true;
 	
-	public Message()
+	public Message(Chat from, Chat to, boolean service, int id, int date, boolean out, boolean unread, String text){
+		this.from = from;
+		this.to = to;
+		this.service = service;
+		this.id = id;
+		this.date = date;
+		this.out = out;
+		this.unread = unread;
+		this.text = text;
+	}
+	
+	// ------ Setter methods -------
+	
+	public void setFrom(Chat from){
+		this.from = from;
+	}
+	
+	public void setTo(Chat to){
+		this.to = to;
+	}
+	
+	public void setService(boolean service){
+		this.service = service;
+	}
+	
+	public void setID(int id){
+		this.id = id;
+	}
+	
+	public void setDate(int date){
+		this.date = date;
+	}
+	
+	public void setOutgoing(boolean out){
+		this.out = out;
+	}
+	
+	public void setUnread(boolean unread){
+		this.unread = unread;
+	}
+	
+	public void setText(String text){
+		this.text = text;
+	}
+	
+	// ------ Getter methods ------
+	
+	public Chat getFrom(){
+		return from;
+	}
+	
+	public Chat getTo(){
+		return to;
+	}
+	
+	public boolean getService(){
+		return service;
+	}
+	
+	public int getID(){
+		return id;
+	}
+	
+	public int getDate(){
+		return date;
+	}
+	
+	public boolean getOutgoing(){
+		return out;
+	}
+	
+	public boolean getUnread(){
+		return unread;
+	}
+	
+	public String getText(){
+		return text;
+	}
 }
