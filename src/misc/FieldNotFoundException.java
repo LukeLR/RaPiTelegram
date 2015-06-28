301103cee6c955bce2b5dc35670845d1a12632e0
Lukas Rose <lukas.rose@gmx.de>
2015-06-29 00:50:22 +0200

    Introducing FieldNotFoundException

---
+++ src/misc/FieldNotFoundException.java
@@ -0,0 +1,11 @@
+package misc;
+
+public class FieldNotFoundException extends Exception {
+	public FieldNotFoundException(){
+		
+	}
+	
+	public FieldNotFoundException(String message){
+		super(message);
+	}
+}
