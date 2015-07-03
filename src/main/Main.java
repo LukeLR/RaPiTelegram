d966ab29066a3ff961c0d8408a3eac270e8d6afa
Lukas Rose <lukas.rose@gmx.de>
2015-07-03 02:23:20 +0200

    Removing Server functionalities

--- src/main/Main.java
+++ src/main/Main.java
@@ -8,11 +8,11 @@ import network.NetworkNotifier;
 public class Main {
 	public static void main(String[] args){
 		Notifier n = new Notifier();
-		NetworkServer ns = new NetworkServer(n, 1234);
+//		NetworkServer ns = new NetworkServer(n, 1234);
 		NetworkClient nc = new NetworkClient(n, "localhost", 1234);
-		ns.start();
-		n.setServer(ns); //gets executed, because NetworkListener is a Thread
-		nc.send("Test!");
-		nc.send("Asdf!");
+//		ns.start();
+//		n.setServer(ns); //gets executed, because NetworkListener is a Thread
+//		nc.send("Test!");
+//		nc.send("Asdf!");
 	}
 }
