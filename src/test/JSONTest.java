98d9f53fa0ef37b9d77543e68e75d7024d2a9b58
Lukas Rose <lukas.rose@gmx.de>
2015-07-07 09:24:33 +0200

    License header added

--- src/test/JSONTest.java
+++ src/test/JSONTest.java
@@ -1,3 +1,21 @@
+/**
+ * This file is part of LukeUtils.
+ *
+ * LukeUtils is free software: you can redistribute it and/or modify
+ * it under the terms of the cc-by-nc-sa (Creative Commons Attribution-
+ * NonCommercial-ShareAlike) as released by the Creative Commons
+ * organisation, version 3.0.
+ *
+ * LukeUtils is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY.
+ *
+ * You should have received a copy of the cc-by-nc-sa-license along
+ * with this LukeUtils. If not, see
+ * <https://creativecommons.org/licenses/by-nc-sa/3.0/legalcode>.
+ *
+ * Copyright Lukas Rose 2013 - 2015
+ */
+
 package test;
 
 import logging.Logger;
