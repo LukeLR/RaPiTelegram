2446197db7e0c7b31b022044aeac71abbb019b3b
Lukas Rose <lukas.rose@gmx.de>
2015-08-02 00:09:03 +0200

    Fixes in Account.class for compatibility with Thread

--- src/misc/Account.java
+++ src/misc/Account.java
@@ -4,29 +4,45 @@ import java.io.Serializable;
 
 import logging.Logger;
 
-public class Account implements Serializable{
+public class Account extends Thread implements Serializable{
 	private String name = "null";
-	private int id = -1;
+	private int accountID = -1;
+	private int accountState = 0;
+	
+	public static int STATE_LOGGEEDOUT = 0;
+	public static int STATE_LOGGEDIN = 1;
+	
+	public Account(String name, int id, int accountState){
+		this.name = name;
+		this.accountID = accountID;
+		this.accountState = accountState;
+	}
 	
 	public Account(String name, int id){
 		this.name = name;
-		this.id = id;
+		this.accountID = accountID;
+		this.accountState = 0;
 	}
 	
 	public Account(int id){
 		this.name = "null";
-		this.id = id;
+		this.accountID = accountID;
+		this.accountState = 0;
 	}
 	
-	public void setName(String name){
+	public void setAccountName(String name){
 		this.name = name;
 	}
 	
-	public void setID(int id){
-		this.id = id;
+	public void setAccountID(int id){
+		this.accountID = accountID;
 	}
 	
-	public String getName(){
+	public void setAccountState(int accountState){
+		this.accountState = accountState;
+	}
+	
+	public String getAccountName(){
 		if (name.equals("null")){
 			return null;
 		} else {
@@ -34,13 +50,17 @@ public class Account implements Serializable{
 		}
 	}
 	
-	public int getID(){
-		return id;
+	public int getAccountID(){
+		return accountID;
+	}
+	
+	public int getAccountState(){
+		return accountState;
 	}
 	
 	public boolean equals(Object compare){
 		try{
-			return ((Account) compare).getID() == getID();
+			return ((Account) compare).getAccountID() == getAccountID();
 		} catch (Exception ex){
 			Logger.logMessage('W', this, "Comparing failed! Is the comparison object of type 'Account'?");
 			return false;
