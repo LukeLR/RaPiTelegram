f00522a180d1f2d128aa775419cb624b10c49406
Lukas Rose <lukas.rose@gmx.de>
2015-08-17 09:42:13 +0200

    Created exception-package

---
+++ src/exception/PrivilegeNotFoundException.java
@@ -0,0 +1,29 @@
+package exception;
+
+public class PrivilegeNotFoundException extends Exception {
+
+	public PrivilegeNotFoundException() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public PrivilegeNotFoundException(String arg0) {
+		super(arg0);
+		// TODO Auto-generated constructor stub
+	}
+
+	public PrivilegeNotFoundException(Throwable arg0) {
+		super(arg0);
+		// TODO Auto-generated constructor stub
+	}
+
+	public PrivilegeNotFoundException(String arg0, Throwable arg1) {
+		super(arg0, arg1);
+		// TODO Auto-generated constructor stub
+	}
+
+	public PrivilegeNotFoundException(String arg0, Throwable arg1,
+			boolean arg2, boolean arg3) {
+		super(arg0, arg1, arg2, arg3);
+		// TODO Auto-generated constructor stub
+	}
+}
